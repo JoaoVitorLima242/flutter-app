e5e2f337728cdbb0120637f0777fc72ab8dc4b5c
JoaoVitorLima242 <jlima0500@gmail.com>
2023-07-11 21:10:30 -0300

    new gradient

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello world!'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              colors: [Color.fromARGB(255, 244, 148, 116), Color(0xfff8b500)],
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello world!'),
+          ),
         ),
       ),
     ),
