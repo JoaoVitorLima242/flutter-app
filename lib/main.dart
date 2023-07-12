ae419578f3964890ee83c7123765b11d96b32ab0
JoaoVitorLima242 <jlima0500@gmail.com>
2023-07-11 21:14:50 -0300

    adding some style to my text

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,21 @@ void main() {
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               begin: Alignment.topRight,
-              colors: [Color.fromARGB(255, 244, 148, 116), Color(0xfff8b500)],
+              end: Alignment.bottomLeft,
+              colors: [
+                Color.fromARGB(255, 244, 148, 116),
+                Color(0xfff8b500),
+              ],
             ),
           ),
           child: const Center(
-            child: Text('Hello world!'),
+            child: Text(
+              'Hello world!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ),
           ),
         ),
       ),
